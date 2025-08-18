280c7b97f741fda247a7ac25f4113777d33a9936
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-08-18 21:13:49 +0200

    Update discord1.py

--- discord1.py
+++ discord1.py
@@ -7,6 +7,11 @@ WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
 
+    embed = Embed(
+        description="**[DMAT forum link - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=97)**",
+        color=0x3498DB
+    )
+
     embed.set_image(url="attachment://cs elfak.jpg")
-    file = File(image2_path, name="cs elfak.jpg")
-    hook.send("@everyone 📢 BP [main page link - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=97)**", embed=embed, file=file)
+    file = File(image2_path, name="cs elfak")
+    hook.send("@everyone 📢 BP", embed=embed, file=file)
