77f2a1dc8697ceed9fc5da39db0c231ebec72a00
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-08-18 21:08:05 +0200

    Update discord1.py

--- discord1.py
+++ discord1.py
@@ -7,11 +7,6 @@ WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
 
-    embed = Embed(
-        description="**[BP main page link - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=97)**",
-        color=0x3498DB
-    )
-
     embed.set_image(url="attachment://cs elfak.jpg")
     file = File(image2_path, name="cs elfak.jpg")
-    hook.send("@everyone 📢 BP", embed=embed, file=file)
+    hook.send("@everyone 📢 BP [main page link - click here -](https://cs.elfak.ni.ac.rs/nastava/course/view.php?id=97)**", embed=embed, file=file)
